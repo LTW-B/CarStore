8d8a34fc6ea62795e4c68500a6ef218f32e667c2
Dr.Moho <nguyenkhanhan79@gmail.com>
2023-09-06 23:52:05 +0700

    Chinh lai trang linh kien

--- resources/views/home/linhkien.blade.php
+++ resources/views/home/linhkien.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
 @section('title', $linhkienData['title'])
 @section('sect4')
+<div class="row">
     @foreach ($linhkienData['products'] as $product)
-    
-            <div class="col-md-4 col-lg-3 col-sm-12 mb-2 row d-flex  ">
-                <div class="card">
-                    {{-- --}}
-                    <img src=" {{ route('upload.show', ['filename' => $product['hinhanh']]) }}" alt="image" class="card-img-top img-card">
-
-                    <div class="card-body text-center">
-
-                        {{--{{route('itemusedata.detail', ['iditem'=> $product-> getId() ] )}}  --}}
-                        <a href=" " class="btn btn-primary text-white">
-                            {{ $product-> getName() }}
-                        </a>
-                    </div>
+        <div class="col-md-4 col-lg-3 col-sm-12 mb-2 row d-flex">
+            <div class="card">
+                {{-- Set a fixed height for the image container --}}
+                <div class="image-container" style="height: 200px; overflow: hidden;">
+                    <img src="{{ route('upload.show', ['filename' => $product['hinhanh']]) }}" alt="image" class="card-img-top img-card">
+                </div>
+                <div class="card-body text-center">
+                    <a href="#" class="btn btn-primary text-white">
+                        {{ $product->getName() }}
+                    </a>
                 </div>
             </div>
-        
+        </div>
     @endforeach
-    </div>
+</div>
 @endsection
