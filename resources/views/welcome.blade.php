a182c0b3d1a93f418d696f9aa7e0efd871bf57c6
Khanh <nguyenkhanhan79@gmail.com>
2023-09-25 23:00:00 +0700

    chỉnh sửa nội dung trang home phần booking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -556,64 +556,7 @@
     <!-- Fact End -->
 
 
-    <!-- Booking Start -->
-    <div class="container-fluid bg-secondary booking my-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container">
-            <div class="row gx-5">
-                <div class="col-lg-6 py-5">
-                    <div class="py-5">
-                        <h1 class="text-white mb-4">Nhà cung cấp Dịch vụ Sửa chữa Ô tô uy tín và chất lượng hàng đầu </h1>
-                        <p class="text-white mb-0">Chúng tôi tự hào là nhà cung cấp dịch vụ sửa chữa ô tô được chứng nhận
-                            và đã nhận nhiều giải thưởng. Với kinh nghiệm nhiều năm, chúng tôi cam kết mang đến cho bạn
-                            những dịch vụ sửa chữa chất lượng và uy tín.</p>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="bg-primary h-100 d-flex flex-column justify-content-center text-center p-5 wow zoomIn"
-                        data-wow-delay="0.6s">
-                        <h1 class="text-white mb-4">Đặt lịch Dịch vụ</h1>
-                        <form>
-                            <div class="row g-3">
-                                <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="Họ và Tên của bạn"
-                                        style="height: 55px;">
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <input type="email" class="form-control border-0" placeholder="Email của bạn"
-                                        style="height: 55px;">
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <select class="form-select border-0" style="height: 55px;">
-                                        <option selected>Chọn Dịch vụ</option>
-                                        <option value="1">Kiểm tra chuẩn đoán</option>
-                                        <option value="2">Sữa chữa và bảo dưỡng động cơ</option>
-                                        <option value="3">Thay thế lốp xe</option>
-                                        <option value="4">Thay dầu máy </option>
-                                        <option value="5">Khác (vui lòng thêm mô tả trong yêu cầu đặc biệt) </option>
-                                    </select>
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <div class="date" id="date1" data-target-input="nearest">
-                                        <input type="text" class="form-control border-0 datetimepicker-input"
-                                            placeholder="Ngày Dịch vụ" data-target="#date1" data-toggle="datetimepicker"
-                                            style="height: 55px;">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <textarea class="form-control border-0" placeholder="Yêu cầu Đặc biệt"></textarea>
-                                </div>
-                                <div class="col-12">
-                                    <button class="btn btn-secondary w-100 py-3" type="submit">Đặt Ngay</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Booking End -->
-
+  
     <!-- Đội ngũ của chúng tôi -->
     <div class="container-xxl py-5">
         <div class="container">
